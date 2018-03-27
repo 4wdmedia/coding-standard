35a8da760621abdae245c1e42fb5b4028e6010e1
Robert Vock <robert.vock@4wdmedia.de>
2018-03-27 08:40:50 +0200

    Fixed some false posititives for EmptyStatementSniff

--- ForwardMedia/Sniffs/CodeAnalysis/EmptyStatementSniff.php
+++ ForwardMedia/Sniffs/CodeAnalysis/EmptyStatementSniff.php
@@ -37,6 +37,20 @@ class EmptyStatementSniff implements Sniff {
 		}
 
 		if ($previousToken && in_array($previousToken['code'], [T_SEMICOLON, T_OPEN_CURLY_BRACKET, T_CLOSE_CURLY_BRACKET, T_OPEN_TAG])) {
+			if ($previousToken['code'] === T_CLOSE_CURLY_BRACKET) {
+				if (isset($previousToken['scope_condition']) && $tokens[$previousToken['scope_condition']]['code'] === T_CLOSURE) {
+					// ignore semicolon after closing bracket of closure
+					return;
+				}
+
+				$openBracket = $tokens[$previousToken['bracket_opener']];
+				$beforeOpening = $phpcsFile->findPrevious(Tokens::$emptyTokens, $previousToken['bracket_opener'] - 1, null, true);
+				if ($beforeOpening && $tokens[$beforeOpening] && $tokens[$beforeOpening]['code'] === T_OBJECT_OPERATOR) {
+					// code like $this->{$var . "string"};
+					return;
+				}
+			}
+
 			// Get token identifier.
 			$name  = strtoupper($token['content']);
 			$error = 'Useless semicolon detected';
